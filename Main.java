459fc765625abd17e2e3de3ac20bbaa17a1f2f15
sainath-b4 <168454764+sainath-b4@users.noreply.github.com>
2024-05-16 15:32:38 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public String hello(){
+        return "hello"
+;    }
+static void hello1(){
+    System..out.println(
+        "hello "
+    )
+}
+public static void main(){
+    Main m1= new Main();
+    m1.hello;
+    Main.hello1
+}
+
+}
